5f19cd530f404658e2aa949d2303f1fe462ae6fe
Sergey <segadev85@gmail.com>
2024-10-07 02:37:26 +0300

    "storage"

--- cmd/url-shortener/main.go
+++ cmd/url-shortener/main.go
@@ -1,8 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"log/slog"
+	"os"
 	"url_shortener/internal/config"
+	"url_shortener/internal/lib/logger/sl"
+	"url_shortener/internal/storage/sqlite"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
@@ -10,14 +19,44 @@ func main() {
 	// TODO: init config: cleanenv
 
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
 
 	// TODO: init logger: slog
 
+	log := setupLogger(cfg.Env)
+	log.Info("starting url-shortener", slog.String("env", cfg.Env))
+	log.Debug("debug message are enabled")
+
 	// TODO: init storage: sqlite
+
+	storage, err := sqlite.New(cfg.StoragePath)
+	if err != nil {
+		log.Error("failed to init storage", sl.Err(err))
+		os.Exit(1)
+	}
+
+	_ = storage
 
 	// TODO: init router: chi, "chi renderer"
 
 	// TODO: run server:
 
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -8,21 +8,23 @@ import (
 )
 
 type Config struct {
-	Env         string `yaml:"env" env-default:"local" env-required:"true"`
+	Env         string `yaml:"env" env-default:"local"`
 	StoragePath string `yaml:"storage_path" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
 type HTTPServer struct {
-	Address     string        `yaml:"address" env-default:"localhost:8080" env-required:"true"`
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
 	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
 	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
 }
 
 func MustLoad() *Config {
-	configPath := os.Getenv("CONFIG_PATH")
+	//configPath := os.Getenv("CONFIG_PATH")
+	configPath := "./config/local.yaml"
+
 	if configPath == "" {
-		log.Fatalln("CONFIG_PATH is not set")
+		log.Fatal("CONFIG_PATH is not set")
 	}
 	if _, err := os.Stat(configPath); os.IsNotExist(err) {
 		log.Fatalf("config file does not exist: %s", configPath)
